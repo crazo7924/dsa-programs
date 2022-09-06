0c1dfb6477d20095ee625744adfaa02d1e0fb085
Bharat Dev Burman <bharat.singh7924@gmail.com>
2022-09-06 11:10:28 +0530

    Minmax: add comments and close the scanner
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- MinMax.java
+++ MinMax.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class MinMax {
-    int[] find(int[] list, int i, int j) {
+    int[] find(int[] list, int start, int end) {
         int min, max;
-        if (i == j) {
-            max = min = list[i];
-        } else if (i == j - 1) {
-            if (list[i] > list[j]) {
-                max = list[i];
-                min = list[j];
+        if (start == end) { // small problem with 1 elements
+            max = min = list[start];
+        } else if (start == end - 1) { // small problems with 2 elements
+            if (list[start] > list[end]) {
+                max = list[start];
+                min = list[end];
             } else {
-                min = list[i];
-                max = list[j];
+                min = list[start];
+                max = list[end];
             }
-        } else {
-            int mid = (i + j) / 2;
-            int[] minmax1 = find(list, i, mid);
-            int[] minmax2 = find(list, mid + 1, j);
+        } else { // not a small problem. perform divide and conquer recursively.
+            int mid = (start + end) / 2;
+            int[] minmax1 = find(list, start, mid);
+            int[] minmax2 = find(list, mid + 1, end);
 
             if (minmax1[0] < minmax2[0])
                 min = minmax1[0];
             else
                 min = minmax2[0];
 
-            if (minmax1[1] >minmax2[1])
+            if (minmax1[1] > minmax2[1])
                 max = minmax1[1];
             else
                 max = minmax2[1];
@@ -39,5 +39,6 @@ public class MinMax {
         int[] list = input.getArray(scanner);
         int[] minmax = m.find(list, 0, list.length - 1);
         System.out.println("Minimum is: " + minmax[0] + " and Maximum is " + minmax[1]);
+        scanner.close();
     }
 }
